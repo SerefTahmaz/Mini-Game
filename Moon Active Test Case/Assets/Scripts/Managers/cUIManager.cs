ad47d8fc4141f9623b97859298103a01e51db4be
SerefTahmaz <58826666+SerefTahmaz@users.noreply.github.com>
2023-10-06 17:53:21 +0300

    [Fixed] Transitions

--- Moon Active Test Case/Assets/Scripts/Managers/cUIManager.cs
+++ Moon Active Test Case/Assets/Scripts/Managers/cUIManager.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 
 public class cUIManager : cSingleton<cUIManager>
 {
-    [SerializeField] private Page m_StartPage;
-    [SerializeField] private List<cView> m_Pages;
+    [SerializeField] private Page m_StartView;
+    [SerializeField] private List<cView> m_Views;
 
     private void Awake()
     {
-        ShowPage(m_StartPage);
+        ShowPage(m_StartView);
     }
 
     public void ShowPage(Page page)
     {
-        m_Pages[(int)page].Activate();
+        m_Views[(int)page].Activate();
     }
     
     public void HidePage(Page page)
     {
-        m_Pages[(int)page].Deactivate();
+        m_Views[(int)page].Deactivate();
     }
 }
 
